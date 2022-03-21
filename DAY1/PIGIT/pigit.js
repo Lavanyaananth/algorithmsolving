1fde1cceb815fc65f7bccf5114f84efc79c8265d
lavanyaananth <lavanyaanand.11@gmail.com>
2022-03-21 18:31:27 +0530

    day 2

--- DAY1/PIGIT/pigit.js
+++ DAY1/PIGIT/pigit.js
@@ -10,13 +10,3 @@ const pigit = (str) => {
 };
 console.log(pigit("Hello world !"));
 console.log(pigit("s12!"));
-// console.log("pig it");
-// let splitted = str.split(" ");
-// console.log(splitted);
-// let pigLatin = [];
-// splitted.map((word) =>
-//   /([a-zA-Z])/.test(word)
-//     ? pigLatin.push(word.substr(1) + word[0] + "ay")
-//     : pigLatin.push(word)
-// );
-// return pigLatin.join(" ");
